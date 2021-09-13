586cc6cc578272e61b436de2fbfa8c9ca9383b5a
wjun <wj2247689442@gmail.com>
2021-09-13 08:42:58 +0800

    v1.0

--- src/main/java/day03/Flink09_Transform_Partition.java
+++ src/main/java/day03/Flink09_Transform_Partition.java
@@ -7,16 +7,16 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 public class Flink09_Transform_Partition {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(4);
+        //env.setParallelism(4);
 
         DataStreamSource<String> socketDS = env.socketTextStream("localhost", 1111);
 
-        SingleOutputStreamOperator<String> DS = socketDS.map(x -> x).setParallelism(2);
+        SingleOutputStreamOperator<String> DS = socketDS.map(x -> x);
 
-        //DS.forward().print("forward");
-        //DS.global().print("global");
-        //DS.broadcast().print("broadcast");
-        //DS.keyBy(x -> x).print("keyBy");
+        DS.forward().print("forward");
+        DS.global().print("global");
+        DS.broadcast().print("broadcast");
+        DS.keyBy(x -> x).print("keyBy");
         DS.print("map").setParallelism(2);
         DS.shuffle().print("shuffle");
         DS.rebalance().print("rebalance");
